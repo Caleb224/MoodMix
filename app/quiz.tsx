cd4340e1bc74cb739e6658941222d9cba0083b9e
Caleb224 <caleb.beere@gmail.com>
2024-03-25 16:04:02 -0400

    Update Mood Quiz

--- app/quiz.tsx
+++ app/quiz.tsx
@@ -1,4 +1,6 @@
+import { Slider } from '@miblanchard/react-native-slider';
 import { AnimatePresence, MotiText, MotiView, ScrollView } from 'moti';
+import { useState } from 'react';
 import { Pressable, Text, View } from 'react-native';
 
 let emotions = [
@@ -29,17 +31,19 @@ let emotions = [
 ];
 
 export default function ModalScreen() {
-
+  const [selectedEmotion, setSelectedEmotion] = useState("");
+  const [moodValue, setMoodValue] = useState<number[]>([0.5]);
+  const [energyValue, setEnergyValue] = useState<number[]>([0.5]);
+  const [tempoValue, setTempoValue] = useState<number[]>([0.5]);
 
   return (
-    <View className='flex-1 justify-start items-start bg-background px-5 py-5'>
-      <ScrollView>
+      <ScrollView className='flex-1 bg-background overflow-hidden p-5' contentContainerStyle={{alignItems: 'flex-start', justifyContent: 'center'}}>
         <AnimatePresence>
           <View className='flex flex-row items-center'>
-            <Text className='text-text font-bold text-xl'>
+            <Text className='text-text font-bold text-2xl'>
               What{" "}
               <MotiView from={{translateY: 0}} animate={{translateY: 3}} transition={{loop: true, duration: 1000, delay: 100, type: 'timing'}} className='inline-block'>
-                <Text className='text-primary font-bold text-xl'>emotion</Text>
+                <Text className='text-primary font-bold text-2xl'>emotion</Text>
               </MotiView>
               {" "} 
               are you feeling right now?</Text>
@@ -50,16 +54,59 @@ export default function ModalScreen() {
             <Pressable
             className='px-4 bg-primary py-1 rounded-xl mx-2'
               key={emotion}>
-                <Text className='text-text font-bold text-lg'>{emotion}</Text>
+                <Text className='text-tertiary font-semibold text-md'>{emotion}</Text>
             </Pressable>
             )
           )}
         </View>
-        <Text className='text-text font-bold text-xl my-3'>Generation Parameters</Text>
-        <View className='flex flex-col justify-start items-center w-full bg-secondary rounded-xl h-48'>
-
+        <Text className='text-text font-bold text-2xl my-3'>Generation Parameters</Text>
+        <View className='flex flex-col justify-start items-center bg-secondary rounded-xl h-min w-full mb-28'>
+          <View className='w-full p-5'>
+            <Text className='text-text font-semibold text-lg'>Mood: <Text className='text-primary font-light text-lg'>{moodValue[0].toPrecision(1)}</Text></Text>
+            <View className='flex flex-row items-center justify-center'>
+              <Text className='text-text font-semibold text-lg'>0</Text>
+              <Slider value={moodValue} 
+                onValueChange={(value) => setMoodValue(value)}
+                step={0.1} 
+                containerStyle={{width: '80%', marginHorizontal: 20}}
+                trackStyle={{backgroundColor: '#153B44'}}
+                minimumTrackTintColor='#C6DE41'
+                thumbTintColor='#C6DE41' 
+                />
+              <Text className='text-text font-semibold text-lg'>1</Text>
+            </View>
+          </View>
+          <View className='w-full p-5'>
+            <Text className='text-text font-semibold text-lg'>Energy: <Text className='text-primary font-light text-lg'>{energyValue[0].toPrecision(1)}</Text></Text>
+            <View className='flex flex-row items-center justify-center'>
+              <Text className='text-text font-semibold text-lg'>0</Text>
+              <Slider value={energyValue} 
+                onValueChange={(value) => setEnergyValue(value)}
+                step={0.1} 
+                containerStyle={{width: '80%', marginHorizontal: 20}}
+                trackStyle={{backgroundColor: '#153B44'}}
+                minimumTrackTintColor='#C6DE41'
+                thumbTintColor='#C6DE41' 
+                />
+              <Text className='text-text font-semibold text-lg'>1</Text>
+            </View>
+          </View>
+          <View className='w-full p-5'>
+            <Text className='text-text font-semibold text-lg'>Tempo: <Text className='text-primary font-light text-lg'>{tempoValue[0].toPrecision(1)}</Text></Text>
+            <View className='flex flex-row items-center justify-center'>
+              <Text className='text-text font-semibold text-lg'>0</Text>
+              <Slider value={tempoValue} 
+                onValueChange={(value) => setTempoValue(value)}
+                step={0.1} 
+                containerStyle={{width: '80%', marginHorizontal: 20}}
+                trackStyle={{backgroundColor: '#153B44'}}
+                minimumTrackTintColor='#C6DE41'
+                thumbTintColor='#C6DE41' 
+                />
+              <Text className='text-text font-semibold text-lg'>1</Text>
+            </View>
+          </View>
         </View>
       </ScrollView>
-    </View>
   );
 }
