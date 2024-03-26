a5394977b569977df9866d9237bfb8345aab7a19
Caleb224 <caleb.beere@gmail.com>
2024-03-26 13:41:00 -0400

    Update styling

--- app/(tabs)/favourites.tsx
+++ app/(tabs)/favourites.tsx
@@ -6,28 +6,31 @@ import PlayList from '@/lib/types/Playlist';
 import PlaylistDisplayer from '@/components/displayers/PlaylistDisplayer';
 import { SafeAreaView } from 'moti';
 import { FontAwesome } from '@expo/vector-icons';
+import { Surface } from 'react-native-paper';
 
 export default function FavouriteScreen() {
   const {favouritePlaylists, addFavourite, removeFavourite} = usePlaylists();
   return (
     <View className='flex-1 justify-center items-center bg-background'>
       <SafeAreaView className='w-full flex-1'>
-        <Text className='text-text font-bold text-xl p-5'>Your Favourites</Text>
-        <FlatList
-        data={favouritePlaylists}
-        keyExtractor={(item: PlayList) => item.name + item.emotion}
-        renderItem={(item) => {
-          let playlist = item.item;
-          let showFavourite = (favouritePlaylists?.indexOf(playlist) > -1);
-          return (
-            <View className='flex flex-row items-center justify-around w-full pb-5'>
-              <PlaylistDisplayer playlist={playlist}/>
-              <Pressable onPress={showFavourite ? () => removeFavourite(playlist) : () => addFavourite(playlist)}>
-                <FontAwesome name={showFavourite ? 'heart' : 'heart-o'} size={18} color="#C6DE41"/>
-              </Pressable>
-            </View>
-          )
-        }}/>
+        <Surface className="h-full w-full bg-transparent" elevation={2}>
+          <Text className='text-text font-bold text-xl p-5'>Your Favourites</Text>
+          <FlatList
+          data={favouritePlaylists}
+          keyExtractor={(item: PlayList) => item.name + item.emotion}
+          renderItem={(item) => {
+            let playlist = item.item;
+            let showFavourite = (favouritePlaylists?.indexOf(playlist) > -1);
+            return (
+              <View className='flex flex-row items-center justify-around w-full pb-5'>
+                <PlaylistDisplayer playlist={playlist}/>
+                <Pressable onPress={showFavourite ? () => removeFavourite(playlist) : () => addFavourite(playlist)}>
+                  <FontAwesome name={showFavourite ? 'heart' : 'heart-o'} size={18} color="#C6DE41"/>
+                </Pressable>
+              </View>
+            )
+          }}/>
+        </Surface>
       </SafeAreaView>
     </View>
   );
